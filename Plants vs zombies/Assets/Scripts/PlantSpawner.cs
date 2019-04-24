87c1084a0ccb68e32e0cbc28ca1b488f86d7c6e0
pau795 <pau_795@hotmail.com>
2019-04-24 13:59:20 +0200

    inital commit

---
+++ Plants vs zombies/Assets/Scripts/PlantSpawner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantSpawner : MonoBehaviour
+{
+    public GameObject plant;
+    private bool used;
+    private Color color;
+    void OnMouseEnter()
+    {
+        color = GetComponent<Renderer>().material.color;
+        if (!used) GetComponent<Renderer>().material.color = Color.green;
+        else GetComponent<Renderer>().material.color = Color.red;
+    }
+    void OnMouseExit()
+    {
+        GetComponent<Renderer>().material.color = color;
+    }
+    void OnMouseDown()
+    {
+        if (!used)
+        {
+            GameObject obj = (GameObject)Instantiate(plant, transform.position, transform.rotation);
+            GetComponent<Renderer>().material.color = Color.red;
+            used = true;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        used = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
