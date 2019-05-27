98504902e47e151c5726ff45249ca4ca3e89016f
fvalles <frrnvlls@gmail.com>
2019-05-27 15:46:36 +0200

    Fixed time when: Game to Menu and Menu to Game

--- Plants vs zombies/Assets/Scripts/MainMenu.cs
+++ Plants vs zombies/Assets/Scripts/MainMenu.cs
@@ -5,6 +5,7 @@ public class MainMenu : MonoBehaviour
 {
     public void Jugar()
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
     
